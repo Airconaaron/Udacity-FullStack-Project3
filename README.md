7dad844a079b160224a9e5fca78fbbf9a017d4e7
Aaron Pang <aaron@193-4.priv23.nus.edu.sg>
2017-08-18 13:41:44 +0800

    changed README and separated function calls

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
-#Udacity Project 3
+# Udacity Project 3
 
-This project is an exploration of SQL databases through `psql` commands.
+This project is an exploration of SQL. The database here is a PostgreSQL database for a fictional news website.
+The news database contains a articles, authors, and log table. 
 
-# Instructions
-Simply run the command
-```
-python sql_queries.py
-```
-which will both print out the output of the specifies queries and put them into a text file called `output.txt`
+The questions we thus want to answer using `python` and the `psycopg2` library are:
+### 1. What are the most popular three articles of all time? 
+### 2. Who are the most popular article authors of all time? 
+### 3. On which days did more than 1% of requests lead to errors?
+
+## Requirements
+* Python2
+* Vagrant
+* VirtualBox
+
+## Instructions
+1. Install Vagrant and VirtualBox through [these steps](https://classroom.udacity.com/nanodegrees/nd004/parts/8d3e23e1-9ab6-47eb-b4f3-d5dc7ef27bf0/modules/bc51d967-cb21-46f4-90ea-caf73439dc59/lessons/5475ecd6-cfdb-4418-85a2-f2583074c08d/concepts/14c72fe3-e3fe-4959-9c4b-467cf5b7c3a0)
+2. Next navigate to an appropriate directory and run `vagrant up` then `vagrant ssh` to start and then access our vm. 
+3. Next download [this file](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip) called `newsdata.sql` and place it in a file accessible to the vm.
+4. To now setup the database run the command `psql -d news -f newsdata.sql`
+Verify the installation by connecting to the database via `psql -d news` and running `\d`. You should see our authors, articles, and log tables. Run `\d TABLENAME` to see their specific schema. 
+5. Finally to get the desired output run `python sql_queries.py`. This shoudl both print out the answers to the above questions and place them in a file called `output.txt`
+
--- sql_queries.py
+++ sql_queries.py
@@ -3,88 +3,128 @@
 
 import psycopg2
 
-# connecting to database
-db = psycopg2.connect("dbname=news")
-f = open('output.txt', 'w')
-c = db.cursor()
+DATABASE = "dbname=news"
+OUTPUT = "output.txt"
+f = None
 
-# 1. What are the most popular three articles of all time?
 
-'''
-This query first counts the number of successful http queries
-and groups them together.
-next we'll match them to the appropriate article titles from their slugs
-and show only the top 3
-'''
-c.execute(
+def db_connect():
+    """ Creates and returns a connection to the database defined by DBNAME,
+        as well as a cursor for the database.
+
+        Returns:
+            db, c - a tuple. The first element is a connection to the database.
+                    The second element is a cursor for the database.
+    """
+
+    db = psycopg2.connect(DATABASE)
+    c = db.cursor()
+    return db, c
+
+
+def execute_query(query):
+    """execute_query takes an SQL query as a parameter.
+        Executes the query and returns the results as a list of tuples.
+        args:
+            query - an SQL query statement to be executed.
+
+       returns:
+           A list of tuples containing the results of the query.
+    """
+
+    db, c = db_connect()
+    c.execute(query)
+    return c.fetchall()
+
+
+def print_top_articles():
+    """Prints out the top 3 articles of all time."""
+    query = '''
+    SELECT articles.title, log_new.views
+    FROM articles JOIN
+        (SELECT path, count(*) AS views
+        FROM log
+        WHERE (status = '200 OK')
+        AND path != '/'
+        GROUP BY path) AS log_new
+    ON log_new.path = concat('/article/', articles.slug)
+    ORDER BY log_new.views DESC
+    LIMIT 3
     '''
-    select articles.title, log_new.views from
-    articles join
-    (select path, count(*) as views
-    from log where (status = '200 OK') and
-    path != '/' group by path) as log_new
-    on log_new.path = concat('/article/', articles.slug)
-    order by log_new.views desc
-    limit 3
+    results = execute_query(query)
+
+    q = "1. What are the most popular three articles of all time? \n"
+    print q
+    f.write(q)
+
+    for item in results:
+        string1 = "{} -- {} views \n".format(item[0], item[1])
+        print string1
+        f.write(string1)
+    f.write("\n")
+
+
+def print_top_authors():
+    """Prints a list of authors ranked by article views."""
+    query = '''
+    SELECT authors.name, sum(articles.views)
+    FROM authors JOIN
+        (SELECT articles.author, log_new.views
+        FROM articles JOIN
+            (SELECT path, count(*) AS views
+            FROM log
+            WHERE (status = '200 OK')
+            AND path != '/'
+            GROUP BY path) AS log_new
+        ON log_new.path = concat('/article/', articles.slug)
+        ORDER BY log_new.views DESC) AS articles
+    ON authors.id = articles.author
+    GROUP BY authors.id
     '''
-    )
+    results = execute_query(query)
 
-q1 = c.fetchall()
+    q = "2. Who are the most popular article authors of all time? \n"
+    print q
+    f.write(q)
 
-# now print both to I/O and write to file.
-print "Question 1:"
-f.write("Question 1: \n")
-for item in q1:
-    print item
-    f.write("{} -- {} views \n".format(item[0], item[1]))
+    for author in results:
+        string2 = "{} -- {} views\n".format(author[0], author[1])
+        print string2
+        f.write(string2)
+    f.write("\n")
 
-# 2. Who are the most popular article authors of all time?
-print "Question 2:"
-f.write("Question2: \n")
 
-'''
-This query uses our previous query in a join statement.
-It then connects each article to the author and sums up
-their aggregate views.
-'''
-c.execute(
+def print_errors_over_one():
+    """Prints out the days where more than 1%
+    of logged access requests were errors."""
+    query = '''
+    SELECT date, percent
+    FROM
+        (SELECT date,
+        (CAST(db_counts.bad AS float) * 100/ CAST(db_counts.total AS FLOAT))
+        AS percent
+        FROM
+            (SELECT count(*) AS total,
+            count(CASE WHEN status != '200 OK' THEN 'x' ELSE NULL END) AS bad,
+            date(time) FROM log GROUP BY date(time)) AS db_counts)
+            AS db_percent
+    WHERE percent > 1
     '''
-    select authors.name, sum(articles.views) from
-    authors join (select articles.author, log_new.views from
-    articles join (select path, count(*) as views
-    from log where (status = '200 OK') and
-    path != '/' group by path) as log_new
-    on log_new.path = concat('/article/', articles.slug)
-    order by log_new.views desc) as articles
-    on authors.id = articles.author
-    group by authors.id
-    '''
-    )
-q2 = c.fetchall()
+    results = execute_query(query)
 
-for author in q2:
-    print author
-    f.write("{} -- {} views\n".format(author[0], author[1]))
+    q = "3. On which days did more than 1% of requests lead to errors? \n"
+    print q
+    f.write(q)
 
-# 3. On which days did more than 1% of requests lead to errors?
-print "Question 3:"
-f.write("Question 3: \n")
+    for date in results:
+        string3 = "{0:%B %d, %Y} -- {1}% errors".format(date[0], date[1])
+        print string3
+        f.write(string3)
+    f.write("\n")
 
-c.execute(
-    '''
-    select date, percent from
-    (select date, (db_counts.bad * 100/ db_counts.total) as percent,
-    bad, total from (select count(*) as total,
-    count(case when status != '200 OK' then 'x' else NULL end) as bad,
-    date(time) from log group by date(time)) as db_counts) as db_percent
-    where percent > 1
-    '''
-    )
-q3 = c.fetchall()
-
-for date in q3:
-    print date
-    f.write("{} -- {} errors".format(date[0], date[1]))
-
-f.close()
-db.close()
+if __name__ == '__main__':
+    f = open(OUTPUT, 'w')
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
+    f.close()
